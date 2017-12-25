b35380f1eec25dfe93c9418434befb60b14d242f
Ioannis Kokkinis <info@upg.gr>
2017-12-25 11:34:20 +0200

    Updated strategy2.php.

--- strategy2.php
+++ strategy2.php
@@ -2,7 +2,7 @@
    $coin = "DOGE"; //what will be the base coin to play to. Effectively this bounds the screen to the DOGE market in cryptopia and only coins in this market.
    $coincap = 20; //how many coins to keep from $coin (not to play them)
    $hours = 48; //how many hours of market data to analyze
-   $buyifabove = 5; // when a coin increased $buyifabove % in the last $hours hours will play (buy)
+   $buyifabove = 10; // when a coin increased $buyifabove % in the last $hours hours will play (buy)
    $targetprofit = 0.20; //target profit to make (sell order)
    $coinbet = 30; //increments of $coin to play
    $targetcoinration = 100; //only play on coins that have last price less than $targetcoinration value than the $coin
