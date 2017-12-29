5a4759a23d587bf291c0f4145feddad70755a36d
Ioannis Kokkinis <info@upg.gr>
2017-12-30 01:56:44 +0200

    Updated index-cli_test.php.

--- index-cli_test.php
+++ index-cli_test.php
@@ -1,6 +1,6 @@
 <?php
 $strategy = $argv[1];
-$strategy_arr = explode("_",$strategy_arr);
+$strategy_arr = explode("_",$strategy);
 $coin = $strategy_arr[0];
 $coincap = $strategy_arr[1];
 $hours = $strategy_arr[2];
