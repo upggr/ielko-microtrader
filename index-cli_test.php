18f65ac44b9e5120cb2105016b01ee624c30ffd2
Ioannis Kokkinis <info@upg.gr>
2017-12-30 01:56:15 +0200

    Updated index-cli_test.php.

--- index-cli_test.php
+++ index-cli_test.php
@@ -1,5 +1,15 @@
 <?php
-$strategy = $argv[1];;
-include $strategy.'.php';
+$strategy = $argv[1];
+$strategy_arr = explode("_",$strategy_arr);
+$coin = $strategy_arr[0];
+$coincap = $strategy_arr[1];
+$hours = $strategy_arr[2];
+$buyifabove = $strategy_arr[3];
+$targetprofit = $strategy_arr[4];
+$coinbet = $strategy_arr[5];
+$targetcoinration =  $strategy_arr[6];
+$lowvolume = $strategy_arr[7];
+$exludecoins = array("MEOW","MCRN");
+
 include 'core_test.php';
  ?>
