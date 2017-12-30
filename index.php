809fb78eb3d43ef07d55400570afc5438d5af0ea
Ioannis Kokkinis <info@upg.gr>
2017-12-30 02:11:02 +0200

    update strategies - no files needed

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 $strategy = $_GET['strategy'];
-include $strategy.'.php';
+$strategy_arr = explode("_",$strategy);
+$coin =  ltrim($strategy_arr[0], '0');
+$coincap =  ltrim($strategy_arr[1], '0');
+$hours =  ltrim($strategy_arr[2], '0');
+$buyifabove =  ltrim($strategy_arr[3], '0');
+$targetprofit =  ltrim($strategy_arr[4], '0');
+$coinbet =  ltrim($strategy_arr[5], '0');
+$targetcoinration =  ltrim($strategy_arr[6], '0');
+$lowvolume = ltrim($strategy_arr[7], '0');
+$exludecoins = array("MEOW","MCRN");
+include 'core_test.php';
 include 'core.php';
  ?>
