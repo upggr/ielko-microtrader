d631654ddef96afe5d3cf47ba96476b0cdf30e54
Ioannis Kokkinis <info@upg.gr>
2017-12-31 00:53:59 +0200

    Updated io.php.

--- ielko-microtrader-strategies/io.php
+++ ielko-microtrader-strategies/io.php
@@ -39,7 +39,7 @@ while ($row = mysqli_fetch_assoc($result)) {
   $therealamount = $row['real_amount'];
   $thegoodamount = $row['good_amount'];
   $thetime = $row['timestamp'];
-  $myIndex = $theid.':'.$theapikey;
+  $myIndex = $theid.':'.$thestrategy;
 
 //  if ($previousprice> 0) {
   if (!isset($aaData[$myIndex])) {
