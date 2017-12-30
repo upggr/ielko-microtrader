137266cf1f32ffcc793856afea6d84b4c0539688
Ioannis Kokkinis <info@upg.gr>
2017-12-31 01:23:11 +0200

    Updated io.php.

--- ielko-microtrader-strategies/io.php
+++ ielko-microtrader-strategies/io.php
@@ -25,7 +25,7 @@ db_query($sql) or die(db_error());
           break;
 
           case 'get_data':
-$sql = "select * from `stats` where timestamp BETWEEN SUBDATE(CURDATE(), INTERVAL 1 MONTH) AND NOW();";
+$sql = "select * from `stats` where apikey = 'ZWZjNzE5ZDY5MzAxNDBmMTk0NGJjYzNj' and strategy ='000000NZDT_0000000010_0000000048_0000000010_0000000.20_0000000030_0000000100_0000000010' and timestamp BETWEEN SUBDATE(CURDATE(), INTERVAL 1 MONTH) AND NOW();";
 $sql = preg_replace("/(?<=[A-Za-z0-9])(')(?=[A-Za-z0-9])/", "\'", $sql);
 $result = db_query($sql);
 if ($result === false) {return false;}
@@ -43,11 +43,6 @@ while ($row = mysqli_fetch_assoc($result)) {
   $thetime = $row['timestamp'];
   $myIndex = $theid;
   if (!isset($aaData[$myIndex])) {
-      $aaData[$myIndex][$thestrategy]['apikey'] = $theapikey;
-      $aaData[$myIndex][$thestrategy]['real_amount'] = $therealamount;
-      $aaData[$myIndex][$thestrategy]['good_amount'] = $thegoodamount;
-      $aaData[$myIndex][$thestrategy]['time'] = $thetime;
-  //    $labelsarr[$myIndex][$thestrategy][$theapikey]['time']= $thetime;
       array_push($labelsarr, $thetime);
       array_push($seriesarr_r, $therealamount);
       array_push($seriesarr_g, $thegoodamount);
@@ -61,13 +56,8 @@ $labels = array('labels' => $labelsarr);
 $series_r = array('series_r' => $seriesarr_r);
 $series_g = array('series_g' => $seriesarr_g);
 $merger = array_merge($labels, $series_r,$series_g);
-$aaData = array_values($aaData);
-
-
-
 header('Content-Type: application/json');
 echo json_encode($merger);
-//echo '<pre>';print_r($merger);echo '</pre>';
 db_close();
 
             break;
