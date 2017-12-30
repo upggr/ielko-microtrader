cf609d10713dd7d21f89e865622baea1fb94a543
Ioannis Kokkinis <info@upg.gr>
2017-12-30 15:35:30 +0200

    Updated io.php.

--- ielko-microtrader-strategies/io.php
+++ ielko-microtrader-strategies/io.php
@@ -3,8 +3,6 @@ include('config.php');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-set_time_limit(0);
-ini_set('memory_limit', '-1');
 header('Access-Control-Allow-Origin: *');
 startio($_GET);
 
@@ -16,7 +14,7 @@ function startio()
           echo 'test';
           break;
 
-      case 'submit_data':
+        case 'submit_data':
         $apikey =  $_GET['apikey'];
         $strategy =  $_GET['strategy'];
         $real_amount =  $_GET['real_amount'];
