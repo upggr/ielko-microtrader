efe5df86ae8a4aeff0eb28ca5a7de9b94702e7c4
Ioannis Kokkinis <info@upg.gr>
2018-01-25 12:50:16 +0200

    Updated core_test.php.

--- core_test.php
+++ core_test.php
@@ -6,7 +6,7 @@ $analyzer = "https://electronicgr.com/cryptobot/ielko-microtrader-strategies/";
 try {
    $ct = New Cryptopia($API_SECRET_CRYPTOPIA, $API_KEY_CRYPTOPIA);
    $mycoinbalance = $ct->getCurrencyBalance( $coin );
-   echo $mycoinbalance."\n";
+   echo "=========COIN BALANCE : ".$mycoinbalance."=======\n";
    $coinpool_all = array();
    $coinpool_price_target = array();
    $coinpool_in_order = array();
